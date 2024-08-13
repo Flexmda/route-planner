e232b40c0b5d17945692adf79b5f6533ea911391
crackkhe <157005065+crackkhe@users.noreply.github.com>
2024-08-13 11:41:38 -0500

    google maps API

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,20 +1,24 @@
-// src/MapComponent.js
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
+// Estilo del contenedor del mapa
 const containerStyle = {
   width: '100%',
   height: '400px'
 };
 
+// Centro del mapa
 const center = {
   lat: -3.745,
   lng: -38.523
 };
 
-function MapComponent() {
+// Componente del mapa
+const MapComponent = () => {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAx-KvgSzNq08UufHtPKLro5TcJL9NtLdw">
+    <LoadScript
+      googleMapsApiKey="AIzaSyDcSZ2P9SYCjQeQxEmbY2VfiJIO2bUII6s" // Tu clave API aquí
+    >
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
