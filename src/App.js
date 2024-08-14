d8a600ecdd64849270947021de2f95006c8c8cc5
crackkhe <157005065+crackkhe@users.noreply.github.com>
2024-08-13 20:06:56 -0500

    update app.js y app.css

--- src/App.js
+++ src/App.js
@@ -13,26 +13,33 @@ function App() {
   const [horarioTarde, setHorarioTarde] = useState('');
   const [direccionTarde, setDireccionTarde] = useState('');
   const [students, setStudents] = useState([]);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Nuevo estado para controlar el Sidebar
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true); // Cambia esto a `true` para mostrar el Sidebar por defecto
 
   useEffect(() => {
     const fetchStudents = async () => {
-      const estudiantes = await getEstudiantes();
-      setStudents(estudiantes);
+      try {
+        const estudiantes = await getEstudiantes();
+        setStudents(estudiantes || []); // Asegúrate de que estudiantes no sea undefined
+      } catch (error) {
+        console.error('Error fetching students:', error);
+      }
     };
 
     fetchStudents();
   }, []);
 
   const handleAddEstudiante = async () => {
-    await addEstudiante(nombre, direccionManana, horarioTarde, direccionTarde);
-    // Actualizar la lista de estudiantes después de agregar uno nuevo
-    const estudiantes = await getEstudiantes();
-    setStudents(estudiantes);
-    setNombre('');
-    setDireccionManana('');
-    setHorarioTarde('');
-    setDireccionTarde('');
+    try {
+      await addEstudiante(nombre, direccionManana, horarioTarde, direccionTarde);
+      const estudiantes = await getEstudiantes();
+      setStudents(estudiantes || []); // Asegúrate de que estudiantes no sea undefined
+      setNombre('');
+      setDireccionManana('');
+      setHorarioTarde('');
+      setDireccionTarde('');
+    } catch (error) {
+      console.error('Error adding student:', error);
+    }
   };
 
   // Extraer las direcciones de la mañana y tarde
@@ -46,7 +53,7 @@ function App() {
   return (
     <Router>
       <div style={{ display: 'flex', height: '100vh' }}>
-        <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+        <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} students={students} />
         <div style={{ flex: 1, padding: '20px', marginLeft: isSidebarOpen ? '250px' : '0', transition: 'margin-left 0.3s ease' }}>
           <button className="menu-button" onClick={toggleSidebar}>
             {isSidebarOpen ? 'Cerrar Menú' : 'Abrir Menú'}
